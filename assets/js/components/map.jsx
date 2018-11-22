daa47db0ef6f070cce09dfac21f23a1a6dfedf19
okesity <okesity23@gmail.com>
2018-11-22 17:00:50 -0500

    add react map

--- assets/js/components/map.jsx
+++ assets/js/components/map.jsx
@@ -1,89 +1,191 @@
+
 import React, { Component } from 'react';
-//import GoogleMapReact from 'google-map-react';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper, Polyline} from 'google-maps-react';
 
+var polyline = require('@mapbox/polyline');
 
-//const AnyReactComponent = ({ text }) => <div>{text}</div>;
+class MapContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      activeMarker: {},
+      selectedPlace: {},
+      showingInfoWindow: false,
+      showingStops: false,
+      shape: null,
+      vehicles: [{lat: 0, lng: 0}],
+      stops: [{lat: 0, lng: 0}],
+      bounds: null,
+      center: null
+    };
+    this.get_location = this.get_location.bind(this);
+    this.get_shape = this.get_shape.bind(this);
+    this.VehicleMarker = this.VehicleMarker.bind(this);
+    this.get_stops = this.get_stops.bind(this);
 
-export class MapContainer extends Component {
-  state = {
-    showInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {},
-    currentLocation: {},
-    // initialCenter: {
-    //   lat: position.coords.latitude,
-    //   lng: position.coords.longitude,
-    // },
-  };
-
-  loadMap() {
-    if(this.props && this.props.google) {
-      const {lag, lng} = this.state.currentLocation;
-    }
+    this.get_location();
+    this.get_shape();
+    this.get_stops();
+    this.get_vehicles();
   }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   if(Prevprops.google != this.props.google) {
-  //     this.loadMap();
-  //   }
-  //
-  //   if(prevSate.currentLocation != this.state.currentLocation) {
-  //     this.loadMap();
-  //   }
+  // setStateAsync(state) {
+  //   return new Promise((resolve) => {
+  //     this.setState(state, resolve)
+  //   });
   // }
 
   componentDidMount() {
-    if(this.props.centerAroundCurrentLocation) {
-      if(navigator.geolocation && navigator) {
-        navigator.geolocation.getCurrentPosition(
-          pos => {
-            const coords = props.coords;
-            this.setState({
-                 lat: position.coords.latitude,
-                 lng: position.coordes.longitude,
-            });
-          });
-      }
-    }
-    this.loadMap();
+    console.log("refreshing");
+    this.get_vehicles();
+    setInterval(this.get_vehicles.bind(this),5000);  //refresh vehicle data every 1 second
   }
 
-  // getGeoLocation = () => {
-  //   if(navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(
-  //       position => {
-  //         console.log("position coords", position.coords);
-  //         this.setState(
-  //           selectedPlace = {
-  //             lat: position.coords.latitude,
-  //             lng: position.coordes.longitude,
-  //           }
-  //         )
-  //       }
-  //     )
-  //   }
-  //   this.loadMap();
-  // }
+  get_location(){
+    console.log("getting location");
+    var here=this;        //closure
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(function(position) {
+        let centerpoint = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        here.setState({center: centerpoint});
+      },function(){
+          console.log("error getting location");
+        });
+      }
+  }
+
+  //get Green-E shape
+  get_shape(){
+    console.log("getting shape");
+    fetch("https://api-v3.mbta.com/shapes?filter[route]=Green-E")
+      .then(response => response.json())
+      .then(json => {
+          this.set_bounds(json.data);
+      })
+      .catch(error => console.error('Error:', error));
+  }
+
+  //get Green-E stops
+  get_stops(){
+    fetch("https://api-v3.mbta.com/stops?filter[route]=Green-E")
+    .then(response => response.json())
+    .then(json => {
+        let stops = json.data.map(d => d.attributes);
+        let stops_data = stops.map(function(stop){ return {lat: stop.latitude, lng: stop.longitude, name: stop.name, address: stop.address}});
+        this.setState({stops: stops_data});
+    })
+    .catch(error => console.error('Error:', error));
+  }
+
+  //turn polyline to boundary points
+  set_bounds(data){
+    console.log("invoked set bounds");
+    let points = polyline.decode(data[2].attributes.polyline);
+    points = points.map(function(point){ return {lat: point[0], lng: point[1]}; });
+    this.setState({bounds: points});
+  }
+
+  get_vehicles(){
+    console.log("getting vehicles");
+    fetch("https://api-v3.mbta.com/vehicles?filter[route]=Green-E")
+      .then(response => response.json())
+      .then(json => {
+          let v = json.data.map(d => d.attributes);
+          let vehicles_data = v.map(function(vehicle){ return {lat: vehicle.latitude, lng: vehicle.longitude, direction: vehicle.direction_id}});
+          this.setState({vehicles: vehicles_data});
+      })
+      .catch(error => console.error('Error:', error));
+  }
+
+
+  onMarkerClick = (props, marker) =>
+    this.setState({
+      activeMarker: marker,
+      selectedPlace: props,
+      showingInfoWindow: true
+    });
+
+  onInfoWindowClose = () =>
+    this.setState({
+      activeMarker: null,
+      showingInfoWindow: false
+    });
+
+  onMapClicked = () => {
+    if (this.state.showingInfoWindow)
+      this.setState({
+        activeMarker: null,
+        showingInfoWindow: false
+      });
+  };
+
+  VehicleMarker(){
+    console.log("start rendering");
+    let vlist = [];
+    if(this.state.vehicles != null){
+      console.log("rendering vehicles", this.state.vehicles );
+      for(let i=0;i<this.state.vehicles.length;i++){
+        var v = this.state.vehicles[i];
+        console.log("v is",v);
+        vlist.push(<Marker position={{lat: v.lat, lng: v.lng}}/>)
+      }
+
+      return vlist;
+      // this.state.vehicles.map(v => {
+      //   return <Marker position={{lat: v.lat, lng: v.lng}} />
+      //   });
+      // let v=this.state.vehicles[0];
+      // let pos = {lat: v.lat, lng: v.lng};
+      // return <Marker name="avv" onClick={this.onMarkerClick} position={pos} />
+    }
+    else{
+      console.log("getting vs");
+      this.get_vehicles();
+      
+    }
+  }
 
   render() {
+    if (!this.props.loaded) return <div>Loading...</div>;
+      
     return (
-      <Map google={this.props.google} zoom={11} style={{width: '80%', height: '700px', marginLeft: '-170px'}}>
+      <Map
+        className="map"
+        google={this.props.google}
+        onClick={this.onMapClicked}
+        style={{ height: '100%', position: 'relative', width: '100%' }}
+        zoom={14}
+        center={this.state.center}
+        >
+        <Marker name="Current location" onClick={this.onMarkerClick} position={this.state.center} />
+        
+        { //mark vehicles on map
+          this.state.vehicles.map(v => 
+            <Marker position={{lat: v.lat, lng: v.lng}} />)
+        }
 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
+        {
+          this.state.showingStops && this.state.stops.map(v => 
+            <Marker position={{lat: v.lat, lng: v.lng}} />)
+        }
+        
+        <Polyline path={this.state.bounds} strokeColor="#008000"/>
+        <InfoWindow
+          marker={this.state.activeMarker}
+          onClose={this.onInfoWindowClose}
+          visible={this.state.showingInfoWindow}>
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
         </InfoWindow>
       </Map>
     );
   }
 }
 
-
 export default GoogleApiWrapper({
-  apiKey: ("AIzaSyBhLvg_Vwoau_QHkCZVz8XtVvzMW8NX86w")
+apiKey: ("AIzaSyBhLvg_Vwoau_QHkCZVz8XtVvzMW8NX86w")
 })(MapContainer)
